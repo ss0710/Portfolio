747901475daffe01a6b589cc7e0b8bf9486ef244
Sudheer Singh <sudheersid0710@gmail.com>
2020-08-28 14:12:14 +0530

    first commit

---
+++ jvsc.js
@@ -0,0 +1,23 @@
+setInterval(timer,1000);
+
+function timer(){
+	var date=new Date();
+	var time=date.toLocaleTimeString();
+	document.getElementById('clock').innerHTML=time;
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
